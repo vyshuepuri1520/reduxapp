95dd98cbfc94dac42401ba732cc398516d15de02
vyshuepuri1520 <saivyshu1520@gmail.com>
2024-05-10 22:59:30 +0530

    video74completed

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h1>buy</h1>
+      <button className="btn btn-primary mx-3">-</button>
+      Add to cart
+      <button className="btn btn-primary mx-3">+</button>
+
+    </div>
+  )
+}
+
+export default Shop
